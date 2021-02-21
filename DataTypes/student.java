dc10fd3f78508e7e44ee562f023c04eb289bb84a
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 13:58:22 -0600

    Create student.java

---
+++ DataTypes/student.java
@@ -0,0 +1,23 @@
+/*
+an exercise in primitive data types
+*/
+
+public class student
+{
+  public static void main(String[] args)
+  {
+
+    int numClasses = 4; //num_classes is a variable with value 4
+    int age = 16; //age is an integer with value 16
+
+    double credits = 4; //double getting an integer value
+    double gpa = 3.6; //GPA is a variable with value 3.6
+
+    final double HOURS_OF_SCHOOL = 7 //a constant value that doesn't change
+    
+    final int HOURS_IN_DAY = 24; //unmodifiable value
+
+    boolean hasGraduated = false; //hasGraduated is false
+    boolean studentIsHappy = true; //studentIsHappy is true
+  }
+}
