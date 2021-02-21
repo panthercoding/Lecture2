fbf8e98274828da9f0a997f4410d778946ba2d10
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 13:58:55 -0600

    Add files via upload

---
+++ Overflow/gangnamStyle.java
@@ -0,0 +1,11 @@
+public class gangnamStyle
+{
+  public static final int INT_MAX = 2147483647;
+
+  public static void main(String[] args)
+  {
+    int views = 2147483647;
+    views++;
+    System.out.println(views);
+  }
+}
---
+++ Overflow/nuclearGandhi.java
@@ -0,0 +1,11 @@
+public class nuclearGandhi
+{
+  public static final int MIN_BYTE = -128;
+
+  public static void main(String[] args)
+  {
+    byte scoreGandhi = -127;
+    scoreGandhi-=2;
+    System.out.println(scoreGandhi);
+  }
+}
